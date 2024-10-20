ea23015b3a9f0aeb127d98a49d6af8021a24f383
legendy4141 <legendy4141@gmail.com>
2024-10-20 18:57:49 -0400

    Build Welcome Screen

---
+++ src/screens/WelcomeScreen.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { View, Text, StyleSheet, ImageBackground, TouchableOpacity} from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../types/navigation';
+
+// Define the navigation prop type
+type WelcomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'WelcomeScreen'>;
+
+type Props = {
+  navigation: WelcomeScreenNavigationProp;
+};
+
+const WelcomeScreen: React.FC<Props> = ({ navigation }) => {
+    return (
+        <ImageBackground
+            source={require('../assets/images/welcome-screen.png')}  // Background image
+            style={styles.background}
+        >
+            {/* Title Text */}
+            <Text style={styles.titleText}>AI Powered Fitness</Text>
+
+            {/* Subtitle Text */}
+            <Text style={styles.subtitleText}>Let's get Energized.</Text>
+
+            {/* Start Button */}
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                    style={styles.startButton}
+                    onPress={() => navigation.navigate('MainScreen')}
+                >
+                    <ImageBackground
+                    source={require('../assets/images/next-button.png')} // Use your image here
+                    style={styles.imageButton}  // Add appropriate styling
+                     />
+                </TouchableOpacity>
+            </View>
+        </ImageBackground>
+    );
+};
+  
+const styles = StyleSheet.create({
+    background: {
+        display: 'flex',
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    titleText: {
+        fontSize: 56,
+        marginTop: 94,
+        marginLeft: 16,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    subtitleText: {
+        marginTop: 12,
+        marginLeft: 16,
+        fontSize: 24,
+        color: 'white',
+    },
+    buttonContainer: {
+        marginTop: '110%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    startButton: {
+        width: 78,
+        height: 78,
+    },
+    imageButton: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageStyle: {
+        borderRadius: 50,  // Optional: if you want a round button
+    },
+    buttonText: {
+        fontSize: 30,
+        color: '#FFFFFF',
+    },
+});
+
+export default WelcomeScreen;
---
+++ src/types/navigation.ts
@@ -0,0 +1,6 @@
+export type RootStackParamList = {
+    WelcomeScreen: undefined;
+    MainScreen: undefined;
+    RegisterScreen: undefined;
+    CheckEmailScreen: { email: string };
+};
